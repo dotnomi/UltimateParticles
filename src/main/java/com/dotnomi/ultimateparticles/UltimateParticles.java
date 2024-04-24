1ef40950f4893f3c6e523485a034915343d6b688
DotNomi <dommiii1205@gmail.com>
2024-04-24 11:54:51 +0200

    Initial commit

---
+++ src/main/java/com/dotnomi/ultimateparticles/UltimateParticles.java
@@ -0,0 +1,17 @@
+package com.dotnomi.ultimateparticles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UltimateParticles extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
